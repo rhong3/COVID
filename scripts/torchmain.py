b5537541f8d1d09f361923d8ef7a23bc63bfbe37
HONGRUNYU <michaelhong1995@gmail.com>
2020-04-14 12:16:45 -0400

    4.14.2

--- scripts/torchmain.py
+++ scripts/torchmain.py
@@ -17,7 +17,10 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchvision.models as models
 import torchvision.transforms as transforms
+import numpy as np
+from sklearn.metrics import roc_auc_score, roc_curve, average_precision_score, precision_recall_curve
 import Sample_prep
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 
@@ -119,6 +122,7 @@ if __name__ == '__main__':
     # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
 
     # train
+    model.train()
     for epoch in range(ep):
         train_loss = 0
         train_correct = 0
@@ -147,6 +151,10 @@ if __name__ == '__main__':
         # validation
         val_loss = 0
         correct = 0
+        model.eval()
+        predlist = []
+        scorelist = []
+        targetlist = []
         with torch.no_grad():
             # Predict
             for batch_index, batch_samples in enumerate(val_loader):
@@ -160,13 +168,38 @@ if __name__ == '__main__':
                 score = F.softmax(output, dim=1)
                 pred = output.argmax(dim=1, keepdim=True)
                 correct += pred.eq(target.long().view_as(pred)).sum().item()
+                targetcpu = target.long().cpu().numpy()
+                predlist = np.append(predlist, pred.cpu().numpy())
+                scorelist = np.append(scorelist, score.cpu().numpy()[:, 1])
+                targetlist = np.append(targetlist, targetcpu)
             print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
                 val_loss / len(val_loader.dataset), correct, len(val_loader.dataset),
                 100.0 * correct / len(val_loader.dataset)), flush=True)
+            TP = ((predlist == 1) & (targetlist == 1)).sum()
+            TN = ((predlist == 0) & (targetlist == 0)).sum()
+            FN = ((predlist == 0) & (targetlist == 1)).sum()
+            FP = ((predlist == 1) & (targetlist == 0)).sum()
+            print('TP=', TP, 'TN=', TN, 'FN=', FN, 'FP=', FP)
+            print('TP+FP', TP + FP)
+            p = TP / (TP + FP)
+            print('precision', p)
+            p = TP / (TP + FP)
+            r = TP / (TP + FN)
+            print('recall', r)
+            F1 = 2 * r * p / (r + p)
+            acc = (TP + TN) / (TP + TN + FP + FN)
+            print('F1', F1)
+            print('acc', acc)
+            AUC = roc_auc_score(targetlist, scorelist)
+            print('AUC', AUC)
 
     # test
     test_loss = 0
     correct = 0
+    model.eval()
+    predlist = []
+    scorelist = []
+    targetlist = []
     with torch.no_grad():
         # Predict
         for batch_index, batch_samples in enumerate(test_loader):
@@ -180,6 +213,63 @@ if __name__ == '__main__':
             score = F.softmax(output, dim=1)
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.long().view_as(pred)).sum().item()
+            targetcpu = target.long().cpu().numpy()
+            predlist = np.append(predlist, pred.cpu().numpy())
+            scorelist = np.append(scorelist, score.cpu().numpy()[:, 1])
+            targetlist = np.append(targetlist, targetcpu)
         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss / len(test_loader.dataset), correct, len(test_loader.dataset),
             100.0 * correct / len(test_loader.dataset)), flush=True)
+
+        TP = ((predlist == 1) & (targetlist == 1)).sum()
+        TN = ((predlist == 0) & (targetlist == 0)).sum()
+        FN = ((predlist == 0) & (targetlist == 1)).sum()
+        FP = ((predlist == 1) & (targetlist == 0)).sum()
+        print('TP=', TP, 'TN=', TN, 'FN=', FN, 'FP=', FP)
+        print('TP+FP', TP + FP)
+        p = TP / (TP + FP)
+        print('precision', p)
+        p = TP / (TP + FP)
+        r = TP / (TP + FN)
+        print('recall', r)
+        F1 = 2 * r * p / (r + p)
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        print('F1', F1)
+        print('acc', acc)
+        AUC = roc_auc_score(targetlist, scorelist)
+        print('AUC', AUC)
+
+        fpr, tpr, _ = roc_curve(targetlist, scorelist)
+        plt.figure()
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve (area = %0.5f)' % AUC)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC of COVID')
+        plt.legend(loc="lower right")
+        plt.savefig("../Results/{}/out/ROC.png".format(dirr))
+
+        average_precision = average_precision_score(targetlist, scorelist)
+        print('Average precision-recall score: {0:0.5f}'.format(average_precision))
+        plt.figure()
+        f_scores = np.linspace(0.2, 0.8, num=4)
+        for f_score in f_scores:
+            x = np.linspace(0.01, 1)
+            y = f_score * x / (2 * x - f_score)
+            l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
+            plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+        precision, recall, _ = precision_recall_curve(targetlist, scorelist)
+        plt.step(recall, precision, color='b', alpha=0.2,
+                 where='post')
+        plt.fill_between(recall, precision, step='post', alpha=0.2,
+                         color='b')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim([0.0, 1.05])
+        plt.xlim([0.0, 1.0])
+        plt.title('COVID PRC: AP={:0.5f}; Accu={}'.format(average_precision, acc))
+        plt.savefig("../Results/{}/out/PRC.png".format(dirr))
